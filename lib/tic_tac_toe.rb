4d0e2e4e0622f840e232004d5652537d0677e52b
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-07-09 18:53:15 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,7 +32,7 @@ class TicTacToe
   end
   
   def valid_move?(position)
-    !position_taken?(position) && index.between?(0,8)
+    !position_taken?(position) && position.between?(0,8)
   end
   
   def turn_count
